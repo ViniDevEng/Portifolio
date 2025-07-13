5bbc041289c61f255066697e38ed3ffc208c3e62
Lucas Vinicius <vinidev.eng@gmail.com>
2025-07-13 12:44:15 -0300

    Add files via upload

---
+++ js/script_alert_dados.js
@@ -0,0 +1,8 @@
+window.addEventListener('DOMContentLoaded', function () {
+      const popup = document.getElementById('popup');
+      popup.style.display = 'block';
+    });
+
+    function fecharPopup() {
+      document.getElementById('popup').style.display = 'none';
+    }
